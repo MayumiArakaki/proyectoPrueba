77c413750bdec8e674875efca6b503ab18c03554
MayumiArakaki <mayumi.arakakisiccha@gmail.com>
2023-07-08 16:03:50 -0500

    Primer commit

--- proyectoPrueba/ContentView.swift
+++ proyectoPrueba/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("chopper1")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300)
+                .rotationEffect(Angle(degrees: 45))
         }
         .padding()
     }
