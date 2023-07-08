01e18ff015c37a977a6371d3d3f94c2e87d998e0
MayumiArakaki <mayumi.arakakisiccha@gmail.com>
2023-07-08 15:21:51 -0500

    Initial Commit

---
+++ proyectoPrueba/proyectoPruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  proyectoPruebaApp.swift
+//  proyectoPrueba
+//
+//  Created by Enrique Alata on 8/07/23.
+//
+
+import SwiftUI
+
+@main
+struct proyectoPruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
